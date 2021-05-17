1b4b47de90a25415de4710fe40df0ef9ec42fde0
Monica Hill <monica.r.hill@gmail.com>
2021-05-17 13:14:22 -0400

    fix bug with resume link

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, { Component }  from 'react';
+import React, {Component}  from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./pages/home";
 import Portfolio from "./pages/portfolio";
 import Contact from "./pages/contact";
 import "./App.css";
 
+export default function App() {
 
-class App extends Component {
-  render() {
+
     return (
       <Router>
-    <Switch>
-      <Route path='/contact'>
-        <Contact />
-      </Route>
-      <Route path='/portfolio'>
-        <Portfolio />
-      </Route>
-      <Route path='/'>
-        <Home />
-      </Route>
-    </Switch>
+          <Switch>
+              <Route  path='/contact'><Contact /></Route>
+              <Route  path='/portfolio'><Portfolio /></Route>
+              <Route exact path='/'><Home /></Route>
+           </Switch>
   </Router>
-    )
-  }
 
-  
+    )
 }
 
-export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+ import { Link } from 'react-router-dom';
 
 
-function Header( ) {
-  return (
-   
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+function Header() {
+
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 
       <div className="container">
         <a className="navbar-brand logo" href="index.html">Monica R. Hill</a>
@@ -14,13 +14,13 @@ function Header( ) {
         </button>
         <div className="collapse navbar-collapse" id="navbar">
           <div className="navbar-nav ml-auto mt-2 mt-lg-0">
-            <a  className="nav-link active" href="index.html">Home <span class="sr-only">(current)</span> </a>
-            <a  className="nav-link" href="portfolio.html">Portfolio</a>
-            <a  className="nav-link" href="contact.html">Contact</a>
-            <a className="nav-item nav-link dropdown dropdown-toggle " href="images/resume.pdf" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <Link to="/" className="links nav-link active" href="index.html">Home <span class="sr-only">(current)</span> </Link>
+            <Link to="/portfolio"  className="links nav-link" href="portfolio.html">Portfolio</Link>
+            <Link to="/contact"  className="links nav-link" href="contact.html">Contact</Link>
+            <a className="nav-item nav-link dropdown dropdown-toggle " href="/resume.pdf" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Resume</a>
             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item"  href="images/resume.pdf"> Monica Hill: Jr. Web Developer</a>
+              <a className="dropdown-item"  href="/resume.pdf"> Monica Hill: Jr. Web Developer</a>
            
           </div>
         </div>
@@ -28,8 +28,6 @@ function Header( ) {
       </div>
     </nav>
    
-
-  )
-}
-
- export default Header;
+    )
+    }
+  export default Header;
